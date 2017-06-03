625d1b006cb1adaafd8f101997b626e17eca132b
AppAnge1008 <812474076@qq.com>
2017-06-03 18:29:03 +0800

    修改今日运营界面

--- src/main/webapp/app/pages/shop/shop.js
+++ src/main/webapp/app/pages/shop/shop.js
@@ -4,13 +4,11 @@
 (function(){
     'use strict';
     var app=angular.module('catpowerserverApp');
-    app.controller("shopOpController",["$scope","shopopService","coachArrangementService","classesTodayService","traineeTodayService",function($scope,shopopService,coachArrangementService,classesTodayService,traineeTodayService){
+    app.controller("shopOpController",["$scope","shopopService","coachArrangementService","classesTodayService",function($scope,shopopService,coachArrangementService,classesTodayService){
         //今日教练排课
         $scope.coachArrangements = coachArrangementService.loadCoachArrangement();
         //今日售课
         $scope.sellingCoursers = classesTodayService.loadSellingCoursers();
-        //今日学员
-        $scope.traineeTodays = traineeTodayService.loadTraineeToday();
         $scope.page={
             index:0,
             size:10
@@ -25,42 +23,111 @@
         $scope.loadbuycourse=loadbuycourse;
         loadbuycourse();
 
+        //上课状态
+        angular.forEach($scope.coachArrangements,function (item) {
+            if( item.courseState == '已结束'){
+                item.status = false;
+            }else{
+                item.status = true;
+            }
+        });
+        //下课
+        $scope.getOutClass = function (id) {
+            var index = -1;
+            angular.forEach($scope.coachArrangements,function (item,key) {
 
+                if(item.id == id ){
+                    index = key;
+                    item.courseState = '已结束';
+                    if( item.courseState == '已结束'){
+                        item.status = false;
+                    }else{
+                        item.status = true;
+                    }
+                };
+            });
 
+        };
+        //取消教练排课
+        $scope.removeCoachArrangement = function (id) {
+            var index = -1;
+            angular.forEach($scope.coachArrangements,function (item,key) {
+                if(item.id == id ){
+                    index = key;
+                };
+            });
+            if(index != -1){
+                $scope.coachArrangements.splice(index,1);
+            }
+            return $scope.coachArrangements;
+        };
+        //删除学员
+        $scope.removeTrainee = function (id,traineeId) {
+            var index = -1;
+            var count = '';
+            angular.forEach($scope.coachArrangements,function (item,key) {
+                if(item.id == id ){
+                    count = key;
+                    angular.forEach(item.traineeName,function (it,k) {
+                        if(it.traineeId == traineeId ){
+                            index = k;
+                        };
+                    });
+                    if(index != -1){
+                        item.traineeName.splice(index,1);
+                        item.traineeCount --;
+                        if(item.traineeName.length == 0){
+                            $scope.coachArrangements.splice(count,1);
+                        }
+                    }
+                    return item.traineeName;
+                };
 
-        //
-
+            });
+            return $scope.coachArrangements;
+        };
+        console.log($scope.coachArrangements);
     }]);
-    app.service("traineeTodayService",[function () {
-        this.loadTraineeToday = function () {
-            var traineeTodays = [{
-                traineeName1:'张三',
-                traineeName2:'李四',
-                traineeName3:'王五',
 
-            },{
-                traineeName1:'cll'
-            }];
-            return traineeTodays;
-        }
-    }]);
     app.service("coachArrangementService",[function () {
+
         this.loadCoachArrangement = function () {
             var coachArrangements = [{
+                id:1,
                 coachName:'鞠董',
                 courseName:'瑜伽',
                 courseState:'上课中',
                 time:'2017-6-2 18:00',
-                traineeCount:'1',
-                traineeName1:'张三'
-
+                traineeCount:[{traineeId:'111',name:'张三'},{traineeId:'222',name:'李四'},{traineeId:'333',name:'王五'}].length,
+                traineeName:[{traineeId:'111',name:'张三'},{traineeId:'222',name:'李四'},{traineeId:'333',name:'王五'}],
+                status:''
             },{
+                id:2,
+                coachName:'鞠董',
+                courseName:'瑜伽',
+                courseState:'已结束',
+                time:'2017-6-2 18:00',
+                traineeCount:[{traineeId:'444',name:'陈乐乐'},{traineeId:'555',name:'陆丹'},{traineeId:'111',name:'张三'},{traineeId:'222',name:'李四'},{traineeId:'333',name:'王五'}].length,
+                traineeName:[{traineeId:'444',name:'陈乐乐'},{traineeId:'555',name:'陆丹'},{traineeId:'111',name:'张三'},{traineeId:'222',name:'李四'},{traineeId:'333',name:'王五'}],
+                status:''
+            },{
+                id:3,
                 coachName:'鞠董',
                 courseName:'瑜伽',
                 courseState:'上课中',
-                time:'2017-6-2 18:00',
-                traineeCount:'1',
-                traineeName1:'陈乐乐'
+                time:'2017-6-2 20:00',
+                traineeCount:[{traineeId:'111',name:'张三'}].length,
+                traineeName:[{traineeId:'111',name:'张三'}],
+                status:''
+            },{
+                id:4,
+                coachName:'鞠董',
+                courseName:'瑜伽',
+                courseState:'已结束',
+                time:'2017-6-2 21:00',
+                traineeCount:[{traineeId:'111',name:'张三'},{traineeId:'222',name:'李四'},{traineeId:'333',name:'王五'}].length,
+                traineeName:[{traineeId:'111',name:'张三'},{traineeId:'222',name:'李四'},{traineeId:'333',name:'王五'}],
+                status:''
             }];
             return coachArrangements;
         }
