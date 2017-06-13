1a69f1d66e6d019beadeaab64f469997c50b6cdc
AppAnge1008 <812474076@qq.com>
2017-06-13 13:52:51 +0800

    课程维护界面的全部功能

--- src/main/webapp/app/data-maintain/course-maintain/course-maintain-delete.controller.js
+++ src/main/webapp/app/data-maintain/course-maintain/course-maintain-delete.controller.js
@@ -2,8 +2,21 @@
     'use strict';
 
     var app = angular.module('catpowerserverApp');
-    app.controller('courseMaintainDeleteController',function () {
+    app.controller('courseMaintainDeleteController',['$scope','$uibModalInstance', 'dataMaintain', 'Course',function ($scope,$uibModalInstance,dataMaintain,Course) {
+        var vm = this;
+        vm.course = dataMaintain;
+        vm.clear = clear;
+        vm.confirmDelete = confirmDelete;
+        function clear () {
+            $uibModalInstance.dismiss('cancel');
+        }
 
-    });
+        function confirmDelete (id) {
+            Course.delete({id: id},
+                function () {
+                    $uibModalInstance.close(true);
+                });
+        }
+    }]);
 
 })();
