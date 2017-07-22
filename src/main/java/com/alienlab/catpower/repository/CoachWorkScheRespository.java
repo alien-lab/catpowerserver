827fa80662a93675490867d6f17d7a4a3a3eddd7
xiaoleye <1419420280@qq.com>
2017-07-22 10:50:59 +0800

    增加教练排班的对应内容

--- src/main/java/com/alienlab/catpower/repository/CoachWorkScheRespository.java
+++ src/main/java/com/alienlab/catpower/repository/CoachWorkScheRespository.java
@@ -1,10 +1,17 @@
 package com.alienlab.catpower.repository;
 
+import com.alienlab.catpower.domain.Coach;
 import com.alienlab.catpower.domain.CoachWorkSche;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  * Created by 橘 on 2017/6/26.
  */
 public interface CoachWorkScheRespository extends JpaRepository<CoachWorkSche,Long> {
+    //根据教练id查询相对应的排版时间
+    @Query("select a from CoachWorkSche a where a.coach = ?1")
+    List<CoachWorkSche> findCoachWorkScheByCoach(Coach coach);
 }
---
+++ src/main/java/com/alienlab/catpower/service/CoachWorkScheService.java
@@ -0,0 +1,49 @@
+package com.alienlab.catpower.service;
+
+import com.alienlab.catpower.domain.CoachWorkSche;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * user :Master Cll
+ * time :2017/7/22
+ */
+public interface CoachWorkScheService {
+    /**
+     * Save a coachWorkSche.
+     *
+     * @param coachWorkSche the entity to save
+     * @return the persisted entity
+     */
+    CoachWorkSche save(CoachWorkSche coachWorkSche);
+
+    /**
+     *  Get all the coachWorkSche.
+     *
+     *  @param pageable the pagination information
+     *  @return the list of entities
+     */
+    Page<CoachWorkSche> findAll(Pageable pageable);
+
+    /**
+     *  Get the "id" coachWorkSche.
+     *
+     *  @param id the id of the entity
+     *  @return the entity
+     */
+    CoachWorkSche findOne(Long id);
+
+    /**
+     *  Delete the "id" course.
+     *
+     *  @param id the id of the entity
+     */
+    void delete(Long id);
+
+    //根据教练ID查询对应的排版时间
+    List<CoachWorkSche> findCoachWorkScheByCoachId(Long coachId) throws Exception;
+
+}
