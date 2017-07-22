84577c9704ab50b7ed8fdd4af2d2c0eca63ea2e5
xiaoleye <1419420280@qq.com>
2017-07-22 12:42:50 +0800

    增加学员预约的接口

--- src/main/java/com/alienlab/catpower/web/rest/LearnerAppointmentResource.java
+++ src/main/java/com/alienlab/catpower/web/rest/LearnerAppointmentResource.java
@@ -1,6 +1,9 @@
 package com.alienlab.catpower.web.rest;
+import com.alienlab.catpower.domain.LearnerAppointment;
+import com.alienlab.catpower.domain.LearnerCharge;
 import com.alienlab.catpower.service.LearnerAppointmentService;
 import com.alienlab.catpower.web.rest.util.ExecResult;
+import com.alienlab.catpower.web.rest.util.HeaderUtil;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Map;
 
 /**
@@ -40,5 +45,18 @@ public class LearnerAppointmentResource {
             ExecResult er=new ExecResult(false,e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(er);
         }
+    }
+
+    @ApiOperation("学员预约")
+    @PostMapping("/learner-appointment")
+    public ResponseEntity<LearnerAppointment> createLearnerCharge(@RequestBody LearnerAppointment learnerAppointment) throws URISyntaxException {
+        log.debug("REST request to save LearnerCharge : {}", learnerAppointment);
+        if (learnerAppointment.getId() != null) {
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "idexists", "A new learnerCharge cannot already have an ID")).body(null);
+        }
+        LearnerAppointment result = learnerAppointmentService.save(learnerAppointment);
+        return ResponseEntity.created(new URI("/api/learner-appointment/" + result.getId()))
+            .headers(HeaderUtil.createEntityCreationAlert(ENTITY_NAME, result.getId().toString()))
+            .body(result);
     }
 }
